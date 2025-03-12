90e5e1c69b1d3c9121034d35ff3fad2897898f22
Ivan Greqov <idgreq@bk.ru>
2025-03-12 18:59:19 +0300

    Shuffle cards after click

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,10 +1,11 @@
 import { Card } from './Card';
 import images from '../data.json';
+import { shuffle } from '../utils/shuffle';
 
 function CardList({ handleClick }) {
   return (
     <div className="cards-list">
-      {images.map((image) => (
+      {shuffle(images).map((image) => (
         <Card key={image} image={image} handleClick={handleClick} />
       ))}
     </div>
---
+++ src/utils/shuffle.js
@@ -0,0 +1,8 @@
+const shuffle = (array) => {
+  return array
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value);
+};
+
+export { shuffle };
