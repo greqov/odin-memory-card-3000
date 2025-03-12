bdec4642ec4238bfb615482cf7434cb7dc61a11b
Ivan Greqov <idgreq@bk.ru>
2025-03-12 18:35:05 +0300

    Add best score functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CardList } from './components/CardList';
 
 function App() {
   const [selected, setSelected] = useState([]);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    if (selected.length > bestScore) {
+      setBestScore(selected.length);
+    }
+  }, [bestScore, selected]);
 
   function handleClick(item) {
     const hasItem = selected.indexOf(item) >= 0;
@@ -19,7 +26,7 @@ function App() {
   return (
     <div className="container">
       <h1>such memory card!</h1>
-      <div>Best score: 0</div>
+      <div>Best score: {bestScore}</div>
       <div>Current score: {selected.length} </div>
       <CardList handleClick={handleClick} />
     </div>
